51723f293fd159cbc8e8c5656026432139777e63
Mere Ngue <therealmerengue@gmail.com>
2019-04-20 14:50:35 +0200

    Placing location pin.

--- TransportControl/Views/ChooseLocationPage.xaml.cs
+++ TransportControl/Views/ChooseLocationPage.xaml.cs
@@ -8,12 +8,35 @@ namespace TransportControl.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChooseLocationPage : BaseContentPage<ChooseLocationViewModel>
     {
+        public Pin ChosenLocationPin { get; set; }
+
         public ChooseLocationPage()
         {
             InitializeComponent();
 
             map.InitializeWithDefaults();
             map.CameraIdled += OnMapCameraIdled;
+            map.MapLongClicked += OnMapLongClicked;
+
+            ClearBtn.Clicked += OnClearBtnClicked;
+        }
+
+        private void OnClearBtnClicked(object sender, System.EventArgs e)
+        {
+            map.Pins.Clear();
+            ChosenLocationPin = null;
+        }
+
+        private void OnMapLongClicked(object sender, MapLongClickedEventArgs e)
+        {
+            map.Pins.Clear();
+            ChosenLocationPin = new Pin()
+            {
+                Type = PinType.Place,
+                Position = new Position(e.Point.Latitude, e.Point.Longitude),
+                Label = "Chosen location"
+            };
+            map.Pins.Add(ChosenLocationPin);
         }
 
         private void OnMapCameraIdled(object sender, CameraIdledEventArgs e)
