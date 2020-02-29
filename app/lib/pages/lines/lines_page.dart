0d6ea30345cabeeafab708f5748e840e197f410a
therealmerengue <therealmerengue@gmail.com>
2020-03-01 00:47:42 +0100

    List animations setup.

--- app/lib/pages/lines/lines_page.dart
+++ app/lib/pages/lines/lines_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_stream_list/animated_stream_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search_app_bar/search_app_bar.dart';
@@ -27,55 +28,101 @@ class _LinesPageState extends State<LinesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<LinesBloc, LinesState>(
-      builder: (context, state) {
-        return NestedScrollView(
-            controller: _scrollViewController,
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverAppBar(
-                    titleSpacing: 0.0,
-                    title: SearchAppBar<LineListItemState>(
-                      title: Text('Lines'),
-                      searcher: BlocProvider.of<LinesBloc>(context),
-                      filter: (LineListItemState item, String query) => item
-                          .line.symbol
-                          .toLowerCase()
-                          .contains(query.trim().toLowerCase()),
-                    ),
-                    pinned: false,
-                    floating: true,
-                    snap: true,
-                    forceElevated: innerBoxIsScrolled)
-              ];
-            },
-            body: ListView.separated(
-                separatorBuilder: (context, index) =>
-                    Divider(color: Colors.black),
-                itemCount: state.filteredItems.length,
-                itemBuilder: (context, index) {
-                  return LineListItem(
-                      state: state.filteredItems.elementAt(index));
-                }));
-      },
+    return NestedScrollView(
+        controller: _scrollViewController,
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+                titleSpacing: 0.0,
+                title: SearchAppBar<LineListItemState>(
+                  title: Text('Lines'),
+                  searcher: BlocProvider.of<LinesBloc>(context),
+                  filter: (LineListItemState item, String query) => item
+                      .line.symbol
+                      .toLowerCase()
+                      .contains(query.trim().toLowerCase()),
+                ),
+                pinned: false,
+                floating: true,
+                snap: true,
+                forceElevated: innerBoxIsScrolled)
+          ];
+        },
+        body: _lineStatesList(BlocProvider.of<LinesBloc>(context)
+            .map((state) => state.filteredItems)));
+  }
+
+  Widget _lineStatesList(Stream<List<LineListItemState>> lineStatesStream) {
+    return AnimatedStreamList<LineListItemState>(
+      streamList: lineStatesStream,
+      itemBuilder: (LineListItemState lineState, int index,
+              BuildContext context, Animation<double> animation) =>
+          _tile(lineState, index, animation),
+      itemRemovedBuilder: (LineListItemState lineState, int index,
+              BuildContext context, Animation<double> animation) =>
+          _removedTile(lineState, index, animation),
+      equals: (ls1, ls2) => ls1.line.symbol == ls2.line.symbol,
     );
   }
-}
 
-class LineListItem extends StatelessWidget {
-  final LineListItemState state;
-
-  const LineListItem({Key key, this.state}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(state.line.symbol),
-        Column(
-          children: [Text(state.line.dest1), Text(state.line.dest2)],
-        )
-      ],
+  Widget _tile(
+      LineListItemState lineState, int index, Animation<double> animation) {
+    final textStyle = TextStyle();
+    final card = Card(
+      child: ListTile(
+        leading: Checkbox(
+          value: lineState.selected,
+          onChanged: (newValue) => {},
+        ),
+        title: Text(
+          lineState.line.symbol,
+          style: textStyle,
+        ),
+        subtitle: Text(
+          lineState.line.dest1 + ' : ' + lineState.line.dest2,
+          style: textStyle,
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.delete),
+          onPressed: () => {},
+        ),
+      ),
     );
+    return index < 20
+        ? SizeTransition(
+            axis: Axis.vertical,
+            sizeFactor: animation,
+            child: card,
+          )
+        : card;
+  }
+
+  Widget _removedTile(
+      LineListItemState lineState, int index, Animation<double> animation) {
+    final textStyle = TextStyle();
+    final card = Card(
+      child: ListTile(
+        leading: Checkbox(
+          value: lineState.selected,
+          onChanged: null,
+        ),
+        title: Text(
+          lineState.line.symbol,
+          style: textStyle,
+        ),
+        subtitle: Text(
+          lineState.line.dest1 + ' : ' + lineState.line.dest2,
+          style: textStyle,
+        ),
+        trailing: const Icon(Icons.delete),
+      ),
+    );
+    return index < 20
+        ? SizeTransition(
+            axis: Axis.vertical,
+            sizeFactor: animation,
+            child: card,
+          )
+        : card;
   }
 }
