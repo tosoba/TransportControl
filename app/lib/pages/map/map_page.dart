5e1a9177e7cf0f794f7688ee978200d3dce10321
therealmerengue <therealmerengue@gmail.com>
2020-03-02 22:57:28 +0100

    Maps setup.

--- app/lib/pages/map/map_page.dart
+++ app/lib/pages/map/map_page.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Text('map'),
-      color: Colors.red,
+    return Scaffold(
+      body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(52.237049, 21.017532),
+            zoom: 11,
+          )),
     );
   }
 }
