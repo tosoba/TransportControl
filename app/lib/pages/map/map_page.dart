4c16f09a1c0cec1269568a104399208438f49a7f
therealmerengue <therealmerengue@gmail.com>
2020-03-15 18:54:48 +0100

    Keep MapPage alive.

--- app/lib/pages/map/map_page.dart
+++ app/lib/pages/map/map_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: CameraPosition(
-        target: LatLng(52.237049, 21.017532),
-        zoom: 11,
-      ));
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage>
+    with AutomaticKeepAliveClientMixin<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(52.237049, 21.017532),
+          zoom: 11,
+        ));
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
