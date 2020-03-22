9c045936fdcf192dc1bed914b68e1f414a48dbc7
therealmerengue <therealmerengue@gmail.com>
2020-03-22 14:42:24 +0100

    Vehicle animation stage refactor.

--- app/lib/pages/map/vehicle_animation_stage.dart
+++ app/lib/pages/map/vehicle_animation_stage.dart
@@ -4,86 +4,70 @@ import 'package:latlong/latlong.dart';
 import 'package:transport_control/model/vehicle.dart';
 
 class VehicleAnimationStage {
-  final double startLat;
-  final double startLon;
-  final double destLat;
-  final double destLon;
-  final double currentLat;
-  final double currentLon;
+  final LatLng start;
+  LatLng _current;
+  final LatLng dest;
   final String lastUpdate;
   final int animationStartTime;
-  final bool isAnimating;
-
+  bool _isAnimating;
   int _durationMillis;
 
-  final Distance distance = const Distance();
+  static final Distance distance = const Distance();
 
-  VehicleAnimationStage(
-    this.startLat,
-    this.startLon,
-    this.destLat,
-    this.destLon,
-    this.currentLat,
-    this.currentLon,
-    this.lastUpdate,
-    this.animationStartTime,
-    this.isAnimating,
-  ) {
-    final distance = startDestDistance;
-    if (distance < 200)
+  bool get isAnimating => _isAnimating;
+  LatLng get current => _current;
+
+  VehicleAnimationStage.forNewlyLoadedVehicle(Vehicle newlyLoadedVehicle)
+      : start = null,
+        _current = LatLng(newlyLoadedVehicle.lat, newlyLoadedVehicle.lon),
+        dest = null,
+        lastUpdate = newlyLoadedVehicle.lastUpdate,
+        animationStartTime = null,
+        _isAnimating = false,
+        _durationMillis = null;
+
+  VehicleAnimationStage.forUpdatedVehiclePosition(
+    Vehicle updatedVehicle,
+    VehicleAnimationStage previousStage,
+  )   : start = previousStage._current,
+        _current = previousStage._current,
+        dest = LatLng(updatedVehicle.lat, updatedVehicle.lon),
+        lastUpdate = updatedVehicle.lastUpdate,
+        animationStartTime = DateTime.now().millisecondsSinceEpoch,
+        _isAnimating = true {
+    initDurationMillis();
+  }
+
+  void initDurationMillis() {
+    final startDestDistance = distance(start, dest);
+    if (startDestDistance < 200)
       _durationMillis = 500;
-    else if (distance < 500)
+    else if (startDestDistance < 500)
       _durationMillis = 1000;
     else
       _durationMillis = 1500;
   }
 
-  VehicleAnimationStage.forNewlyLoadedVehicle(Vehicle newlyLoadedVehicle)
-      : startLat = null,
-        startLon = null,
-        destLat = null,
-        destLon = null,
-        currentLat = newlyLoadedVehicle.lat,
-        currentLon = newlyLoadedVehicle.lon,
-        lastUpdate = newlyLoadedVehicle.lastUpdate,
-        animationStartTime = null,
-        isAnimating = false;
-
-  VehicleAnimationStage.forUpdatedVehiclePosition(
-    Vehicle updatedVehicle,
-    VehicleAnimationStage previousStage,
-  )   : startLat = previousStage.currentLat,
-        startLon = previousStage.currentLon,
-        destLat = updatedVehicle.lat,
-        destLon = updatedVehicle.lon,
-        currentLat = previousStage.currentLat,
-        currentLon = previousStage.currentLon,
-        lastUpdate = updatedVehicle.lastUpdate,
-        animationStartTime = DateTime.now().millisecondsSinceEpoch,
-        isAnimating = true;
-
   VehicleAnimationStage.nextStage(VehicleAnimationStage previousStage)
-      : startLat = previousStage.startLat,
-        startLon = previousStage.startLon,
-        destLat = previousStage.destLat,
-        destLon = previousStage.destLon,
-        currentLat = previousStage.currentLat,
-        currentLon = previousStage.currentLon,
+      : start = previousStage.start,
+        dest = previousStage.dest,
         lastUpdate = previousStage.lastUpdate,
         animationStartTime = previousStage.animationStartTime,
-        isAnimating = true;
+        _durationMillis = previousStage._durationMillis {
+    final elapsedMillis = previousStage.elapsed;
+    final multiplier = previousStage.interpolation(elapsedMillis);
+    final lng = multiplier * previousStage.dest.longitude +
+        (1 - multiplier) * previousStage.start.longitude;
+    final lat = multiplier * previousStage.dest.latitude +
+        (1 - multiplier) * previousStage.start.latitude;
+    _current = LatLng(lat, lng);
+    _isAnimating = elapsedMillis < _durationMillis;
+  }
 
   int get elapsed => DateTime.now().millisecondsSinceEpoch - animationStartTime;
 
-  double get startDestDistance {
-    return distance(
-      LatLng(startLat, startLon),
-      LatLng(destLat, destLon),
-    );
-  }
-
-  double get interpolation {
-    if (!isAnimating) throw ArgumentError();
-    return (cos(((elapsed / _durationMillis) + 1) * pi) / 2.0) + 0.5;
+  double interpolation(int elapsedMillis) {
+    if (!_isAnimating) throw ArgumentError();
+    return (cos(((elapsedMillis / _durationMillis) + 1) * pi) / 2.0) + 0.5;
   }
 }
