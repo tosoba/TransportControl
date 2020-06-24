8dab43a7ecb23a8bde4f3b5aa8d93dde07036356
Tomasz Osoba <tomasz.osoba@comarch.pl>
2020-06-24 16:38:00 +0200

    LastSearchedPage appbar setup.

--- app/lib/pages/last_searched/last_searched_page.dart
+++ app/lib/pages/last_searched/last_searched_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:transport_control/widgets/text_field_app_bar.dart';
+import 'package:transport_control/widgets/text_field_app_bar_back_button.dart';
 
 enum LastSearchedPageFilterMode { ALL, LINES, LOCATIONS }
 
@@ -11,9 +13,19 @@ class LastSearchedPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final searchFieldFocusNode = useFocusNode();
+    final searchFieldController = useTextEditingController();
+    searchFieldController.addListener(() {});
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
+      appBar: TextFieldAppBar(
+        textFieldFocusNode: searchFieldFocusNode,
+        textFieldController: searchFieldController,
+        hint: "Search items...",
+        leading: TextFieldAppBarBackButton(searchFieldFocusNode),
+      ),
       body: Center(child: Text('Last searched')),
     );
   }
